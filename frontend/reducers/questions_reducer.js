889eb94d71d78f65e83cd83105860fb086a0b33f
Alexander <alexcrisel4@gmail.com>
2020-01-13 19:37:11 -0800

    Adds questions reducer

--- frontend/reducers/questions_reducer.js
+++ frontend/reducers/questions_reducer.js
@@ -1,22 +1,22 @@
 import { RECEIVE_QUESTIONS, RECEIVE_QUESTION, REMOVE_QUESTION } from "../actions/quiz_actions";
 
-const quizzesReducer = (state = {}, action) => {
+const questionsReducer = (state = {}, action) => {
   Object.freeze(state);
   let newState
   switch (action.type) {
-    case RECEIVE_QUIZZES:
-      newState = Object.assign({}, state, action.quizzes)
+    case RECEIVE_QUESTIONS:
+      newState = Object.assign({}, state, action.questions)
       return newState
-    case RECEIVE_QUIZ:
-      newState = Object.assign({}, state, { [action.payload.quiz.id]: action.payload.quiz })
+    case RECEIVE_QUESTION:
+      newState = Object.assign({}, state, { [action.payload.question.id]: action.payload.question })
       return newState
     case REMOVE_QUIZ:
       newState = Object.assign({}, state);
-      delete newState[action.payload.quiz.id]
+      delete newState[action.payload.question.id]
       return newState
     default:
       return state;
   }
 }
 
-export default quizzesReducer;
+export default questionsReducer;
